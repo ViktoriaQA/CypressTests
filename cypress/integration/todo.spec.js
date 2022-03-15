8e6c06f9c618790b31b26b25aa833813fbbb7cfe
vitafranc93@gmail.com <vitafranc93@gmail.com>
2022-03-16 01:33:29 +0200

    update 16/03/22 t:01:33

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -11,7 +11,7 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe('example to-do app 16/03/22 01:23', () => {
+describe('example to-do app 16/03/22 01:33', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
